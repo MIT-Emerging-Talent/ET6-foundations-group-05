1f38b76324d91253256d9097b1de9a2f257e7840
Abbiihjah Oyeoku <abbiihjahoyeoku@gmail.com>
2025-01-09 01:54:16 -0500

    i created the function and the test

--- solutions/function_adding_two_integers.py
+++ solutions/function_adding_two_integers.py
@@ -1,1 +1,32 @@
-hello world! 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module provides a utility function for basic arithmetic operations.
+
+Functions:
+    adding_two_integers(num1: int, num2: int) -> int:
+        Calculates the adding_two_integers of two integers.
+
+Example:
+    To use the `adding_two_integers` function:
+    
+    >>> result = adding_two_integers(5, 7)
+    >>> print(result)
+    12
+"""
+def adding_two_integers(self, num1: int, num2: int) -> int:
+    if not isinstance(x, int):
+        raise TypeError("Input must be an integer.")
+    assert -(2**31) <= x <= 2**31 - 1, "Input is outside the valid range."
+    """
+    Calculates the adding_two_integers of two integers.
+
+    Args:
+        num1 (int): The first integer to be added.
+        num2 (int): The second integer to be added.
+
+    Returns:
+        int: The adding_two_integers of num1 and num2.
+    """
+    return num1 + num2
+        
